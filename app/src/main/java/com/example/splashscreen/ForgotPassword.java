fc525b3f31a036bac91f1ac3e64764b87f20d233
Shubh Verma <shubh.verma.19cs@bmu.edu.in>
2021-11-07 14:41:50 +0530

    Login, Register, Forgot Pass done

--- app/src/main/java/com/example/splashscreen/ForgotPassword.java
+++ app/src/main/java/com/example/splashscreen/ForgotPassword.java
@@ -1,14 +1,68 @@
 package com.example.splashscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
-
+    EditText forgetEmail;
+    Button forgetButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        forgetEmail = (EditText) findViewById(R.id.tv_forgotemail);
+        forgetButton = (Button) findViewById(R.id.btn_forgot_reset);
+        FirebaseAuth mAuth;
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+//        forgetTextLink.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                EditText resetMail = new EditText(view.getContext());
+//
+//            }
+//        });
+        forgetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = forgetEmail.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    forgetEmail.setError("Password is required");
+                }
+                mAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+                startLoginPage();
+            }
+        });
+
     }
+
+    public void startLoginPage() {
+        Intent forIntent = new Intent(ForgotPassword.this,LogInActivity.class);
+        startActivity(forIntent);
+    }
+
 }
--- app/src/main/java/com/example/splashscreen/RegisterActivity.java
+++ app/src/main/java/com/example/splashscreen/RegisterActivity.java
@@ -1,13 +1,21 @@
 package com.example.splashscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -30,11 +38,46 @@ public class RegisterActivity extends AppCompatActivity {
         regPass = (EditText) findViewById(R.id.password_register);
         regBtn = (Button) findViewById(R.id.btn_register);
         registerView = (TextView) findViewById(R.id.login_text);
-
+        FirebaseAuth mAuth;
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+//        if(mAuth.getCurrentUser()!=null){
+//            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//            finish();
+//        }
         regBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
+                String email = regMail.getText().toString().trim();
+                String username = regUser.getText().toString().trim();
+                String password = regPass.getText().toString().trim();
+                //trim -> removes extra spaces
+                if(TextUtils.isEmpty(email)){
+                    regMail.setError("Email is required");
+                }
+                if(TextUtils.isEmpty(username)){
+                    regUser.setError("Username is required");
+                }
+                if(TextUtils.isEmpty(password)){
+                    regPass.setError("Password is required");
+                }
+                if(password.length()<6){
+                    regPass.setError("Password length should be greater than 6 characters !!");
+                }
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this,"User Created",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this,"Error !!"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                 startLoginPage();
             }
         });
